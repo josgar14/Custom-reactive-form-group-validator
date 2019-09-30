3ea381ba7900b679e45a73d4c2327c3cd6b443cd
Josue <josgar_14@hotmail.com>
2019-09-29 23:33:06 -0400

    Implementing custom validators on form groups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+function ssnValidator(control: FormControl): { [key: string]: any } {
+	const value: string = control.value || '';
+	const valid = value.match(/^\d{9}$/);
+	return valid ? null : { ssn: true };
+}
+
+function equalValidator({ value }: FormGroup): { [key: string]: any } {
+	const [ first, ...rest ] = Object.keys(value || {});
+	const valid = rest.every((v) => value[v] === value[first]);
+	return valid ? null : { equal: true };
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'CustomFormGroupValidation';
+	formModel: FormGroup;
+
+	constructor(fb: FormBuilder) {
+		this.formModel = fb.group({
+			userName: [ '', Validators.required ],
+			socialSecurity: [ '', ssnValidator ],
+			passwordsGroup: fb.group(
+				{
+					password: [ '', Validators.minLength(5) ],
+					pconfirm: [ '' ]
+				},
+				{ validator: equalValidator }
+			)
+		});
+	}
+
+	onSubmit() {
+		console.log(this.formModel.value);
+	}
 }
